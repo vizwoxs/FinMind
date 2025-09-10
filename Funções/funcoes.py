2743e4052bfc00c330a2652a50d092d6f92b9dee
Vik <vikoliveira.dev@gmail.com>
2025-09-10 20:04:54 -0300

    Update funcoes.py

--- Funções/funcoes.py
+++ Funções/funcoes.py
@@ -4,20 +4,27 @@ import os
 caminho = "login_finmind.json" #arquivo
 
 def carregar_user(): #se o usuario não existir retorna lista vazia
-    if not os.path.exist(caminho):
+    if not os.path.exists(caminho):
         return []
     with open(caminho, "r", encoding="utf-8") as arquivo:
-        dados = json.load(arquivo)
-        return dados.get("usuarios", [])
+        dados = json.load(arquivo) #converter o arquivo em um dicionário em python
+        return dados.get("usuarios", []) #retorna a lista de usuario dentro da chave, se não existir retorna lista vazia
     
-def salvar_user(usuario, senha):
-    usuarios = carregar_user() #se o usuario já existe
+def salvar_user(user, senha):
+    usuarios = carregar_user() #se o usuario já existe, se não retorna false como falha de cadastro
     for usuario in usuarios:
-        if usuario["usuarios"] == usuario:
+        if usuario["usuario"] == user:
             return False #usuario já existe
         
-    usuarios.append({"usuario": usuario,
+    usuarios.append({"usuario": user,
                      "senha": senha})
     with open(caminho, "w", encoding="utf-8") as arquivo:
         json.dump({"usuarios": usuarios}, arquivo, indent=4, ensure_ascii=False)
     return True
+
+def validar_login(user, senha): #carrega usuarios cadastrados
+    usuarios = carregar_user() #percorre a lista verificando
+    for usuario in usuarios:
+        if usuario["usuario"] == user and usuario["senha"] == senha:
+            return True #login valido
+    return "Esse usuario não existe" #login invalido
