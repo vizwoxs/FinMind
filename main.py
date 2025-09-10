87f151be54c2c5d70d7c74102c9f7c86a50d77be
Vik <vikoliveira.dev@gmail.com>
2025-09-09 21:00:19 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import requests as rq
+import pandas as pd
+import openai
+import Funções.funcoes as fun
+import json
+
+#api de inteligência artificial
+openai.api_key = "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"
+
+#api de login
+with open("login_finmind.json", "w", encoding="utf-8") as arquivo:
+    
+
+
+
+
+
+
+
+    
+
